51d19af4ee0207ab01759447196e58f211b7aadf
Rucknium <Rucknium@protonmail.com>
2025-04-14 22:41:13 +0000

    Fix bug in gen.old.new.dsa.rings()

--- CCS-milestone-2/decoyanalysis/R/monte-carlo.R
+++ CCS-milestone-2/decoyanalysis/R/monte-carlo.R
@@ -466,7 +466,7 @@ gen.old.new.dsa.rings <- function(n.rings, beta, old.dsa, new.dsa, real.spend,
   message("Generating new-DSA rings...")
   rings.new <- gen.nonfungible.rings(draw.my.dsa = new.dsa, draw.their.dsa = new.dsa,
     draw.real.spend = real.spend, n = ring.size, beta = beta,
-    C = change.probability, n.rings = n.rings.old,
+    C = change.probability, n.rings = n.rings.new,
     check.data.sorted = check.data.sorted)
   
   list(
